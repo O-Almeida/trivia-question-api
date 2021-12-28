735d6e769fde9094b369d742ed234006120f3b83
O-Almeida <brunof.santos.almeida@gmail.com>
2021-12-28 17:38:34 +0000

    is_true and is_false methods created

--- ui.py
+++ ui.py
@@ -19,6 +19,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Text",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -26,11 +27,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.is_true)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.is_false)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -40,3 +41,11 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def is_true(self):
+        self.quiz.check_answer("true")
+
+    def is_false(self):
+        self.quiz.check_answer("false")
+
+
