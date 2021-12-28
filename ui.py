24ae6af7500524f8e149f9744747ba778839b25b
O-Almeida <brunof.santos.almeida@gmail.com>
2021-12-28 17:51:01 +0000

    minor bug correction

--- ui.py
+++ ui.py
@@ -40,12 +40,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}")
         if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
             self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def is_true(self):
         is_right = self.quiz.check_answer("true")
